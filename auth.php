b622e5ee9caa6dd8384fbca9663a209e78df0b12
root <root@laohu-dev.laohu.org>
2018-08-12 23:08:13 -0500

    Refact auth

--- auth.php
+++ auth.php
@@ -43,14 +43,45 @@ class auth_plugin_earlychildhood extends auth_plugin_base {
         $grant_type = "password";
         $client_key = "1_3bcbxd9e24g0gk4swg0kwgcwg4o8k8g4g888kwc44gcc0gwwk4";
         $client_secret = "4ok2x70rlfokc8g0wws8c8kwcokw80k44sg48goc0ok4w0so0k";
-        $earlychildhood_host = "http://primerainfanciasantiagodecali.org/app/web";
+        $earlychildhood_host = "http://primerainfanciasantiagodecali.org/app/web/oauth/v2/token";
         $user = "admin";
         $password = "admin1234";
 
+        $data = new stdClass();
+        $data->grant_type = $grant_type;
+        $data->client_kety = $client_key;
+        $data->client_secret = $client_secret;
+        $data->user = $user;
+        $data->password = $password;
+
+        $data = json_encode($data);
+
         if( (strlen($earlychildhood_host) > 0) && (strlen($client_key) > 0) && (strlen($client_secret) > 0) ){
+            $file = "/var/www/html/moodle35/auth/earlychildhood/file.txt";
+            $fp = fopen($file, "w");
+            // $url = 'http://api.flickr.com/services/xmlrpc/';
+            $curl_request = curl_init($earlychildhood_host);
+
+            curl_setopt($curl_request, CURLOPT_POST, 1);
+            curl_setopt($curl_request, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($curl_request, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl_request, CURLOPT_FILE, $fp);
+            curl_setopt($curl_request, CURLINFO_HEADER_OUT, true);
             
-            $redirect_url = $earlychildhood_host."/oauth/v2/token?grant_type=".$grant_type."?client_key=".$client_key."&client_secret=".$client_secret."&username=".$user."&password=".$password;
-            header('Location: ' . $redirect_url);
+            $response = curl_exec($curl_request);
+
+            print_r(curl_getinfo($curl_request));
+
+            //  $info = curl_getinfo($curl_request);
+            //  print_r($info['request_header']);
+
+
+            // curl_close($curl_request);
+
+            
+           
+            
+
             die;
         }
     }
@@ -63,96 +94,96 @@ class auth_plugin_earlychildhood extends auth_plugin_base {
         return false;
     }
 
-    function callback_handler() {
+    // function callback_handler() {
 
-        global $CFG, $DB, $SESSION;
+    //     global $CFG, $DB, $SESSION;
 
-        $grant_type = "password";
-        $client_key = "1_3bcbxd9e24g0gk4swg0kwgcwg4o8k8g4g888kwc44gcc0gwwk4";
-        $client_secret = "4ok2x70rlfokc8g0wws8c8kwcokw80k44sg48goc0ok4w0so0k";
-        $earlychildhood_host = "http://primerainfanciasantiagodecali.org/app/web";
+    //     $grant_type = "password";
+    //     $client_key = "1_3bcbxd9e24g0gk4swg0kwgcwg4o8k8g4g888kwc44gcc0gwwk4";
+    //     $client_secret = "4ok2x70rlfokc8g0wws8c8kwcokw80k44sg48goc0ok4w0so0k";
+    //     $earlychildhood_host = "http://primerainfanciasantiagodecali.org/app/web";
         
-        // strip the trailing slashes from the end of the host URL to avoid any confusion (and to make the code easier to read)
-        $earlychildhood_host = rtrim($earlychildhood_host, '/');
+    //     // strip the trailing slashes from the end of the host URL to avoid any confusion (and to make the code easier to read)
+    //     $earlychildhood_host = rtrim($earlychildhood_host, '/');
         
-        // at this stage we have been provided with new permanent token
-        $connection = new BasicOAuth($client_key, $client_secret, $_SESSION['oauth_token'], $_SESSION['oauth_token_secret'], $grant_type);
+    //     // at this stage we have been provided with new permanent token
+    //     $connection = new BasicOAuth($client_key, $client_secret, $_SESSION['oauth_token'], $_SESSION['oauth_token_secret'], $grant_type);
         
-        $connection->host = $earlychildhood_host;
+    //     $connection->host = $earlychildhood_host;
         
-        $connection->accessTokenURL = $earlychildhood_host."/oauth/v2/token";
+    //     $connection->accessTokenURL = $earlychildhood_host."/oauth/v2/token";
         
-        $tokenCredentials = $connection->getAccessToken($_REQUEST['oauth_verifier']);
+    //     $tokenCredentials = $connection->getAccessToken($_REQUEST['oauth_verifier']);
         
-        if(isset($tokenCredentials['oauth_token']) && isset($tokenCredentials['oauth_token_secret'])) {
+    //     if(isset($tokenCredentials['oauth_token']) && isset($tokenCredentials['oauth_token_secret'])) {
         
-            $perm_connection = new BasicOAuth($client_key, $client_secret, $tokenCredentials['oauth_token'],
-                    $tokenCredentials['oauth_token_secret']);
+    //         $perm_connection = new BasicOAuth($client_key, $client_secret, $tokenCredentials['oauth_token'],
+    //                 $tokenCredentials['oauth_token_secret']);
             
-            $account = $perm_connection->get($earlychildhood_host . '/wp-json/wp/v2/users/me?context=edit');
+    //         $account = $perm_connection->get($earlychildhood_host . '/wp-json/wp/v2/users/me?context=edit');
             
-            if(isset($account)) {
-                // firstly make sure there isn't an email collision:
-                if($user = $DB->get_record('user', array('email'=>$account->email))) {
-                    if($user->auth != 'earlychildhood') {
-                        print_error('usercollision', 'auth_earlychildhood');
-                    }
-                }
+    //         if(isset($account)) {
+    //             // firstly make sure there isn't an email collision:
+    //             if($user = $DB->get_record('user', array('email'=>$account->email))) {
+    //                 if($user->auth != 'earlychildhood') {
+    //                     print_error('usercollision', 'auth_earlychildhood');
+    //                 }
+    //             }
                 
-                // check to determine if a user has already been created...     
-                if($user = authenticate_user_login($account->username, $account->username)) {
-                    // TODO update the current user with the latest first name and last name pulled from WordPress?
+    //             // check to determine if a user has already been created...     
+    //             if($user = authenticate_user_login($account->username, $account->username)) {
+    //                 // TODO update the current user with the latest first name and last name pulled from WordPress?
         
-                    if (user_not_fully_set_up($user, false)) {
-                        $urltogo = $CFG->wwwroot.'/user/edit.php?id='.$user->id.'&amp;course='.SITEID;
-                        // We don't delete $SESSION->wantsurl yet, so we get there later
+    //                 if (user_not_fully_set_up($user, false)) {
+    //                     $urltogo = $CFG->wwwroot.'/user/edit.php?id='.$user->id.'&amp;course='.SITEID;
+    //                     // We don't delete $SESSION->wantsurl yet, so we get there later
         
-                    }
-                } else {
-                    require_once($CFG->dirroot . '/user/lib.php');
+    //                 }
+    //             } else {
+    //                 require_once($CFG->dirroot . '/user/lib.php');
                     
-                    // we need to configure a new user account
-                    $user = new stdClass();
+    //                 // we need to configure a new user account
+    //                 $user = new stdClass();
                     
-                    $user->mnethostid = $CFG->mnet_localhost_id;
-                    $user->confirmed = 1;
-                    $user->username = $account->username;
-                    $user->password = AUTH_PASSWORD_NOT_CACHED;
-                    $user->firstname = $account->first_name;
-                    $user->lastname = $account->last_name;
-                    $user->email = $account->email;
-                    $user->description = $account->description;
-                    $user->auth = 'wordpress';
+    //                 $user->mnethostid = $CFG->mnet_localhost_id;
+    //                 $user->confirmed = 1;
+    //                 $user->username = $account->username;
+    //                 $user->password = AUTH_PASSWORD_NOT_CACHED;
+    //                 $user->firstname = $account->first_name;
+    //                 $user->lastname = $account->last_name;
+    //                 $user->email = $account->email;
+    //                 $user->description = $account->description;
+    //                 $user->auth = 'wordpress';
                     
-                    $id = user_create_user($user, false);
+    //                 $id = user_create_user($user, false);
                     
-                    $user = $DB->get_record('user', array('id'=>$id));
-                }
+    //                 $user = $DB->get_record('user', array('id'=>$id));
+    //             }
                 
-                complete_user_login($user);
+    //             complete_user_login($user);
                 
-                if (isset($SESSION->wantsurl) and (strpos($SESSION->wantsurl, $CFG->wwwroot) === 0)) {
-                    $urltogo = $SESSION->wantsurl;    /// Because it's an address in this site
-                    unset($SESSION->wantsurl);
+    //             if (isset($SESSION->wantsurl) and (strpos($SESSION->wantsurl, $CFG->wwwroot) === 0)) {
+    //                 $urltogo = $SESSION->wantsurl;    /// Because it's an address in this site
+    //                 unset($SESSION->wantsurl);
                 
-                } else {
-                    $urltogo = $CFG->wwwroot.'/';      /// Go to the standard home page
-                    unset($SESSION->wantsurl);         /// Just in case
-                }
+    //             } else {
+    //                 $urltogo = $CFG->wwwroot.'/';      /// Go to the standard home page
+    //                 unset($SESSION->wantsurl);         /// Just in case
+    //             }
                 
-                /// Go to my-moodle page instead of homepage if defaulthomepage enabled
-                if (!has_capability('moodle/site:config',context_system::instance()) and !empty($CFG->defaulthomepage) && $CFG->defaulthomepage == HOMEPAGE_MY and !isguestuser()) {
-                    if ($urltogo == $CFG->wwwroot or $urltogo == $CFG->wwwroot.'/' or $urltogo == $CFG->wwwroot.'/index.php') {
-                        $urltogo = $CFG->wwwroot.'/my/';
-                    }
-                }
+    //             /// Go to my-moodle page instead of homepage if defaulthomepage enabled
+    //             if (!has_capability('moodle/site:config',context_system::instance()) and !empty($CFG->defaulthomepage) && $CFG->defaulthomepage == HOMEPAGE_MY and !isguestuser()) {
+    //                 if ($urltogo == $CFG->wwwroot or $urltogo == $CFG->wwwroot.'/' or $urltogo == $CFG->wwwroot.'/index.php') {
+    //                     $urltogo = $CFG->wwwroot.'/my/';
+    //                 }
+    //             }
                 
-                redirect($urltogo);
+    //             redirect($urltogo);
                 
-                exit;
-            }
-        }
-    }
+    //             exit;
+    //         }
+    //     }
+    // }
 
 
 }
