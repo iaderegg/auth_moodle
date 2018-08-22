0fa0997c1d75c60b5325dea0e5a04e942751a035
root <root@laohu-dev.laohu.org>
2018-08-21 21:42:51 -0500

    Se añade URL

--- auth.php
+++ auth.php
@@ -39,7 +39,7 @@ class auth_plugin_earlychildhood extends auth_plugin_base {
         $client_id = "1_3bcbxd9e24g0gk4swg0kwgcwg4o8k8g4g888kwc44gcc0gwwk4";
         $client_secret = "4ok2x70rlfokc8g0wws8c8kwcokw80k44sg48goc0ok4w0so0k";
         $earlychildhood_token = "http://primerainfanciasantiagodecali.org/app/web/oauth/v2/token";
-        $earlychildhood_active_user = "http://primerainfanciasantiagodecali.org/app/web/api/currentUser";
+        $earlychildhood_active_user = "http://primerainfanciasantiagodecali.org/app/web/api/currentUser/nMfCkKXYpsLQ2g==";
         $username = "admin";
         $password = "admin1234";
 
